b78d0ad786aa3c94437be21a20a8f647241f221d
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2023-04-02 17:49:48 +0200

    git update

---
+++ meta/git-update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+WORKSPACE_DIR="$HOME"/WORK/workspace
+GIT_REPO_DIR="$WORKSPACE_DIR"/plants
+
+git config --global pull.ff only
+
+if [ -d "$GIT_REPO_DIR" ]; then
+  echo "Git repo seems to be initialized at: $GIT_REPO_DIR"
+  cd "$GIT_REPO_DIR" || exit 1
+  git reset --hard HEAD
+  git pull
+else
+  echo "Git repo at $GIT_REPO_DIR not initialized. Initializing..."
+  mkdir -p "$WORKSPACE_DIR"
+  cd "$WORKSPACE_DIR" || exit 1
+
+  git clone -q https://github.com/kmazur/plants.git
+  cd "$GIT_REPO_DIR" || exit 1
+fi
