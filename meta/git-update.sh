eaeab51d57023bc0ae8006f2fd5f24fddf95519a
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2023-04-06 15:15:15 +0200

    git-update: clean repo from untracked files

--- meta/git-update.sh
+++ meta/git-update.sh
@@ -9,6 +9,7 @@ if [ -d "$GIT_REPO_DIR" ]; then
   echo "Git repo seems to be initialized at: $GIT_REPO_DIR"
   cd "$GIT_REPO_DIR" || exit 1
   git reset --hard HEAD
+  git clean -x -f
   git pull
 else
   echo "Git repo at $GIT_REPO_DIR not initialized. Initializing..."
