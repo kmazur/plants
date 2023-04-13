263a871995dc75226c1feca02f2c1f87f6f45171
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2023-04-14 00:36:47 +0200

    [Python][Timelapse] Fixed command & sleep

--- python/camera/timelapse.py
+++ python/camera/timelapse.py
@@ -1,8 +1,10 @@
 from subprocess import call
+import time
 
 path = '/home/user/WORK/workspace/plants/python/camera'
 executable = 'take_picture_timestamped.py'
-command = path + '/' + executable
+command = 'python ' + path + '/' + executable
 
 while True:
     call([command], shell=True)
+    time.sleep(10)
