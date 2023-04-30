5d727b0bd08bc52f56ad84fb243d8b44ee879935
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2023-04-30 12:45:17 +0200

    [Shell] Fix xargs for upload_last

--- shell/cron/upload_last.sh
+++ shell/cron/upload_last.sh
@@ -27,7 +27,7 @@ if [ "$MY_IP" = "192.168.0.45" ]; then
   if [ -d "$PHOTO_SOURCE_DIR" ]; then
     mkdir -p "$PHOTO_DEST_DIR"
     cd "$PHOTO_SOURCE_DIR"
-    ls -1athr | grep "$CURRENT_DATE_UNDERSCORE" | tail -n 100 | grep jpg | xargs -0 -I {} cp -n {} "$PHOTO_DEST_DIR"
+    ls -1athr | grep "$CURRENT_DATE_UNDERSCORE" | tail -n 100 | grep jpg | xargs -I {} cp -n {} "$PHOTO_DEST_DIR"
     cd $MONITORING_DIR
     drive push
   fi
@@ -38,7 +38,7 @@ elif [ "$MY_IP" = "192.168.0.206" ]; then
   if [ -d "$VID_SOURCE_DIR" ]; then
     mkdir -p "$VID_DEST_DIR"
     cd "$VID_DEST_DIR"
-    ls -1athr | grep "$CURRENT_DATE" | tail -n 2 | grep mkv | xargs -0 -I {} cp -n {} "$VID_DEST_DIR"
+    ls -1athr | grep "$CURRENT_DATE" | tail -n 2 | grep mkv | xargs -I {} cp -n {} "$VID_DEST_DIR"
     cd $MONITORING_DIR
     drive push
   fi
@@ -46,7 +46,7 @@ elif [ "$MY_IP" = "192.168.0.80" ]; then
   if [ -d "$PHOTO_SOURCE_DIR" ]; then
     mkdir -p "$PHOTO_DEST_DIR"
     cd "$PHOTO_SOURCE_DIR"
-    ls -1athr | grep "$CURRENT_DATE_UNDERSCORE" | tail -n 100 | grep jpg | xargs -0 -I {} cp -n {} "$PHOTO_DEST_DIR"
+    ls -1athr | grep "$CURRENT_DATE_UNDERSCORE" | tail -n 100 | grep jpg | xargs -I {} cp -n {} "$PHOTO_DEST_DIR"
     cd $MONITORING_DIR
     drive push
   fi
