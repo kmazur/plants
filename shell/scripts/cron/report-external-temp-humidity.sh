36532c227948e5b0ba05858c84b7f7b0abea5c90
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2024-03-28 17:02:35 +0100

    [Shell] Fix logging for humidity reporting

--- shell/scripts/cron/report-external-temp-humidity.sh
+++ shell/scripts/cron/report-external-temp-humidity.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python "$REPO_DIR/python/reporting/report-humidity-temp-sensor.py" "$*"
+PROCESS_NAME="report-humidity-temp-sensor"
+LOG_FILE="$LOGS_DIR/$PROCESS_NAME.log"
+
+python "$REPO_DIR/python/reporting/report-humidity-temp-sensor.py" "$*" &>> "$LOG_FILE"
