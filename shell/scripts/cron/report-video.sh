a1a022b444a50a4c9885bc9a654925710f157a61
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2024-03-28 17:20:55 +0100

    [Video] Added cron video

---
+++ shell/scripts/cron/report-video.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# shellcheck source=shell/scripts/lib.sh
+source "$LIB_INIT_FILE"
+ensure_env
+
+MIN_PERIOD="${1:-0}"
+MAX_PERIOD="${2:-300}"
+PERIOD="$MIN_PERIOD"
+
+function update_period() {
+  PERIOD="$(get_scaled_inverse_value "$MIN_PERIOD" "$MAX_PERIOD")"
+}
+
+
+OUTPUT_DIR="$VIDEO_DIR"
+SEGMENT_DURATION_SECONDS="600"
+VID_CONFIG_FILE="$REPO_DIR/shell/scripts/video/video-config.txt"
+
+while true; do
+  if ! is_scale_suspended; then
+    START_DATE_TIME="$(get_current_date_time_compact)"
+
+    FILE_NAME="video_$START_DATE_TIME.h264"
+    FILE_PATH="$OUTPUT_DIR/$FILE_NAME"
+
+    log "Recording segment: $FILE_NAME"
+    libcamera-vid -c "$VID_CONFIG_FILE" -t "${SEGMENT_DURATION}000" -o "$FILE_PATH"
+  else
+    log_warn "Video recording suspended"
+  fi
+
+  update_period
+  log "Period is: $PERIOD s"
+  sleep "$PERIOD"
+
+done
