1ce86bd6e9494c1a0626bc36a4c22f58363dbc25
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2024-03-29 09:32:17 +0100

    [Env] Fix boot config appending

--- shell/scripts/init/init-general.sh
+++ shell/scripts/init/init-general.sh
@@ -3,19 +3,19 @@
 # Disable BlueTooth
 BLUETOOTH_DISABLE="$(sudo cat "/boot/config.txt" | grep "dtoverlay=pi3-disable-bt")"
 if [ -z "$BLUETOOTH_DISABLE" ]; then
-  echo "dtoverlay=pi3-disable-bt" | sudo tee "/boot/config.txt"
+  echo "dtoverlay=pi3-disable-bt" | sudo tee -a "/boot/config.txt"
 fi
 
 # Disable POWER LED
 
 ACT_LED_TRIGGER="$(sudo cat "/boot/config.txt" | grep "dtparam=act_led_trigger=")"
 if [ -z "$ACT_LED_TRIGGER" ]; then
-  echo "dtparam=act_led_trigger=none" | sudo tee "/boot/config.txt"
+  echo "dtparam=act_led_trigger=none" | sudo tee -a "/boot/config.txt"
 fi
 
 ACT_LED_ACTIVELOW="$(sudo cat "/boot/config.txt" | grep "dtparam=act_led_activelow=")"
 if [ -z "$ACT_LED_ACTIVELOW" ]; then
-  echo "dtparam=act_led_activelow=on" | sudo tee "/boot/config.txt"
+  echo "dtparam=act_led_activelow=on" | sudo tee -a "/boot/config.txt"
 fi
 
 # Disable HDMI
