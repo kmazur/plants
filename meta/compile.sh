2fa02fa18064c5c7a89a98e4ee0a479cbe643621
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2024-04-04 09:48:15 +0200

    [C] Fix

--- meta/compile.sh
+++ meta/compile.sh
@@ -3,7 +3,7 @@
 echo "Compiling executables"
 
 cd "$REPO_DIR/shell/scripts/audio/" || exit 1
-gcc -o volume_aggregator volume_aggregator.c -O3
+gcc -o volume_aggregator volume_aggregator.c -O3 -march=native -flto
 mv volume_aggregator "$BIN_DIR"
 
 
@@ -12,5 +12,5 @@ g++ -o light_level light_level.cpp `pkg-config --cflags --libs opencv4` -O3 -mar
 mv light_level "$BIN_DIR"
 
 cd "$REPO_DIR/shell/scripts/video/" || exit 1
-g++ -o motion_detector video_processor.cpp `pkg-config --cflags --libs opencv4` -O3 -march=native -flto
+g++ -o motion_detector motion_detector.cpp `pkg-config --cflags --libs opencv4` -O3 -march=native -flto
 mv motion_detector "$BIN_DIR"
