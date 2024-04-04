30477f98e77a4e25036dd7121e72b494061c3b10
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2024-04-04 10:02:16 +0200

    [C] optimizations

--- meta/compile.sh
+++ meta/compile.sh
@@ -3,14 +3,14 @@
 echo "Compiling executables"
 
 cd "$REPO_DIR/shell/scripts/audio/" || exit 1
-gcc -o volume_aggregator volume_aggregator.c -O3 -march=native -flto
+gcc -o volume_aggregator volume_aggregator.c -Ofast -march=native -flto -finline-functions -funroll-loops -ffast-math
 mv volume_aggregator "$BIN_DIR"
 
 
 cd "$REPO_DIR/shell/scripts/video/" || exit 1
-g++ -o light_level light_level.cpp `pkg-config --cflags --libs opencv4` -O3 -march=native -flto
+g++ -o light_level light_level.cpp `pkg-config --cflags --libs opencv4` -Ofast -march=native -flto -finline-functions -funroll-loops -ffast-math
 mv light_level "$BIN_DIR"
 
 cd "$REPO_DIR/shell/scripts/video/" || exit 1
-g++ -std=c++17 -o motion_detector motion_detector.cpp `pkg-config --cflags --libs opencv4` -O3 -march=native -flto
+g++ -std=c++17 -o motion_detector motion_detector.cpp `pkg-config --cflags --libs opencv4` -Ofast -march=native -flto -finline-functions -funroll-loops -ffast-math
 mv motion_detector "$BIN_DIR"
