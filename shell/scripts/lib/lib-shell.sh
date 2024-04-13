bf289a507af4c49c56b4040fa78155b11d1ebd2f
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2024-04-13 22:46:09 +0200

    fix

--- shell/scripts/lib/lib-shell.sh
+++ shell/scripts/lib/lib-shell.sh
@@ -19,7 +19,7 @@ function restart_cron_process() {
   if [[ "$LINES" -ne "1" ]]; then
     return 2
   fi
-  local PID="$(ps aux | grep "$ENTRY" | tr -s ' ' | cut -f 2 -d ' ')"
+  local PID="$(ps aux | grep "$ENTRY" | grep -v "grep.*$ENTRY" | tr -s ' ' | cut -f 2 -d ' ')"
   local PLINES="$(echo -n "$PID" | grep -c "^")"
   if [[ "$PLINES" -ne "1" ]]; then
     return 3
