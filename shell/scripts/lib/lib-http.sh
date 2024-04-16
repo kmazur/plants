6eb01e711981f88090ae6b62521e2e4dc551919c
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2024-04-17 00:23:10 +0200

    fix

--- shell/scripts/lib/lib-http.sh
+++ shell/scripts/lib/lib-http.sh
@@ -5,9 +5,9 @@ function upload_file() {
   local FILE_TYPE="${2:-image/jpg}"
 
   local FILENAME="$(echo "$FILE" | rev | cut -d '/' -f 1 | rev)"
-  local PATH="$(echo "$FILE" | rev | cut -d '/' -f 2- | rev)"
+  local FILEPATH="$(echo "$FILE" | rev | cut -d '/' -f 2- | rev)"
 
   local HTTP_AUTH="$(get_required_config "http.token")"
-  cd "$PATH"
+  cd "$FILEPATH"
   curl -XPOST "http://34.133.13.235:8089/tmp/?auth=$HTTP_AUTH" --form "file=@$FILENAME;type=$FILE_TYPE"
 }
