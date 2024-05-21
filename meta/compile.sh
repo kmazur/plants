1db6a755ade670d65fd11cfdb04114ad2da7eb86
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2024-05-21 23:15:00 +0200

    test

--- meta/compile.sh
+++ meta/compile.sh
@@ -2,16 +2,48 @@
 
 echo "Compiling executables"
 
+function has_changed() {
+  local FILE="$1"
+  local SHA_FILE="${FILE}.sha256"
+  if [ ! -f "$TMP_DIR/$SHA_FILE" ] || [[ "$(cat "$TMP_DIR/$SHA_FILE")" == "$(sha256sum "$FILE" | cut -d ' ' -f 1)" ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function update_sha() {
+  local FILE="$1"
+  local SHA_FILE="${FILE}.sha256"
+  sha256sum "$FILE" | cut -d ' ' -f 1 > "$TMP_DIR/$SHA_FILE"
+}
+
+
 cd "$REPO_DIR/shell/scripts/audio/" || exit 1
-gcc -o volume_aggregator volume_aggregator.c -Ofast -march=native -flto -finline-functions -funroll-loops -ffast-math
-mv volume_aggregator "$BIN_DIR"
+if has_changed "volume_aggregator.c"; then
+  gcc -o volume_aggregator volume_aggregator.c -Ofast -march=native -flto -finline-functions -funroll-loops -ffast-math
+  mv volume_aggregator "$BIN_DIR"
+  update_sha "volume_aggregator.c"
+fi
+
 
 
 cd "$REPO_DIR/shell/scripts/video/" || exit 1
-g++ -o light_level light_level.cpp `pkg-config --cflags --libs opencv4` -Ofast -march=native -flto -finline-functions -funroll-loops -ffast-math
-mv light_level "$BIN_DIR"
+if has_changed "light_level.cpp"; then
+  g++ -o light_level light_level.cpp `pkg-config --cflags --libs opencv4` -Ofast -march=native -flto -finline-functions -funroll-loops -ffast-math
+  mv light_level "$BIN_DIR"
+  update_sha "light_level.cpp"
+fi
+
 
 cd "$REPO_DIR/shell/scripts/video/" || exit 1
-g++ -std=c++17 -o motion_detector motion_detector.cpp `pkg-config --cflags --libs opencv4` -pthread -Ofast -march=native -flto -finline-functions -funroll-loops -ffast-math
-mv motion_detector "$BIN_DIR"
+if has_changed "motion_detector.cpp"; then
+  g++ -std=c++17 -o motion_detector motion_detector.cpp `pkg-config --cflags --libs opencv4` -pthread -Ofast -march=native -flto -finline-functions -funroll-loops -ffast-math
+  mv motion_detector "$BIN_DIR"
+  update_sha "motion_detector.cpp"
+fi
+
+
+
+
 
