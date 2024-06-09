e73dc946a1452c94ca11ca9b39c21c94a2019022
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2024-06-10 00:15:08 +0200

    fix

--- shell/scripts/video/motion_detector.cpp
+++ shell/scripts/video/motion_detector.cpp
@@ -288,7 +288,6 @@ private:
                         writeMotionDataToFile(motionStartTime, motionEndTime, motionDataList);
                         motionDataList.clear();
                         motionStartTime = -1;
-                        break;
                     }
                 }
 
