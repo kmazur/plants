6fd51eb5bc34b766c70265427bf5cca58a8bc80e
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2024-06-11 16:44:56 +0200

    fix

--- shell/scripts/cron/stage-3-create_24h_timelapse.sh
+++ shell/scripts/cron/stage-3-create_24h_timelapse.sh
@@ -37,13 +37,13 @@ while true; do
   FILE_PATH="$OUTPUT_STAGE_DIR/$FILE_NAME"
 
   if [[ ! -f "$FILE_PATH" ]]; then
-    if ! ffmpeg -framerate "$VIDEO_24_TIMELAPSE_INPUT_FPS" -i "$LATEST_NOT_PROCESSED_PATH" -c:v libx264 -r "$VIDEO_24_TIMELAPSE_OUTPUT_FPS" -pix_fmt yuv420p "$FILE_PATH"; then
+    if ! ffmpeg -y -framerate "$VIDEO_24_TIMELAPSE_INPUT_FPS" -i "$LATEST_NOT_PROCESSED_PATH" -c:v libx264 -r "$VIDEO_24_TIMELAPSE_OUTPUT_FPS" -pix_fmt yuv420p "$FILE_PATH"; then
       continue
     fi
   else
     NEW_FRAME_VIDEO_PATH="$OUTPUT_STAGE_DIR/${MACHINE_NAME}_24_timelapse_processing_new_vid.mp4"
     FRAME_DURATION="0$(echo "scale=4; 1/$VIDEO_24_TIMELAPSE_OUTPUT_FPS" | bc)"
-    if ! ffmpeg -loop 1 -i "$LATEST_NOT_PROCESSED_PATH" -c:v libx264 -t "$FRAME_DURATION" -pix_fmt yuv420p "$NEW_FRAME_VIDEO_PATH"; then
+    if ! ffmpeg -y -loop 1 -i "$LATEST_NOT_PROCESSED_PATH" -c:v libx264 -t "$FRAME_DURATION" -pix_fmt yuv420p "$NEW_FRAME_VIDEO_PATH"; then
       rm "$NEW_FRAME_VIDEO_PATH" 2> /dev/null
       continue
     fi
@@ -55,7 +55,7 @@ while true; do
       echo "file '$NEW_FRAME_VIDEO_PATH'"
     } > "$CONCAT_LIST_PATH"
 
-    if ffmpeg -f concat -safe 0 -i "$CONCAT_LIST_PATH" -c copy "$TMP_OUTPUT_PATH"; then
+    if ffmpeg -y -f concat -safe 0 -i "$CONCAT_LIST_PATH" -c copy "$TMP_OUTPUT_PATH"; then
       if cp -f "$TMP_OUTPUT_PATH" "$FILE_PATH"; then
         echo "$LATEST_NOT_PROCESSED_FILE" >> "$PROCESSED_PATH"
       fi
