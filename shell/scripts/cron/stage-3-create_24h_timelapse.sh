1192ef4265138f6709d4fafc3b93fd553fa2a86b
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2024-06-11 16:45:46 +0200

    fix

--- shell/scripts/cron/stage-3-create_24h_timelapse.sh
+++ shell/scripts/cron/stage-3-create_24h_timelapse.sh
@@ -14,8 +14,7 @@ OUTPUT_STAGE="video/24_timelapse"
 while true; do
   sleep 30
 
-  VIDEO_24_TIMELAPSE_INPUT_FPS="$(get_or_set_config "video.24_timelapse.fps" "4")"
-  VIDEO_24_TIMELAPSE_OUTPUT_FPS="$(get_or_set_config "video.24_timelapse.fps" "12")"
+  VIDEO_24_TIMELAPSE_FPS="$(get_or_set_config "video.24_timelapse.fps" "4")"
 
   MACHINE_NAME="$(get_required_config "name")"
 
@@ -37,12 +36,12 @@ while true; do
   FILE_PATH="$OUTPUT_STAGE_DIR/$FILE_NAME"
 
   if [[ ! -f "$FILE_PATH" ]]; then
-    if ! ffmpeg -y -framerate "$VIDEO_24_TIMELAPSE_INPUT_FPS" -i "$LATEST_NOT_PROCESSED_PATH" -c:v libx264 -r "$VIDEO_24_TIMELAPSE_OUTPUT_FPS" -pix_fmt yuv420p "$FILE_PATH"; then
+    if ! ffmpeg -y -framerate "$VIDEO_24_TIMELAPSE_FPS" -i "$LATEST_NOT_PROCESSED_PATH" -c:v libx264 -r "$VIDEO_24_TIMELAPSE_FPS" -pix_fmt yuv420p "$FILE_PATH"; then
       continue
     fi
   else
     NEW_FRAME_VIDEO_PATH="$OUTPUT_STAGE_DIR/${MACHINE_NAME}_24_timelapse_processing_new_vid.mp4"
-    FRAME_DURATION="0$(echo "scale=4; 1/$VIDEO_24_TIMELAPSE_OUTPUT_FPS" | bc)"
+    FRAME_DURATION="0$(echo "scale=4; 1/$VIDEO_24_TIMELAPSE_FPS" | bc)"
     if ! ffmpeg -y -loop 1 -i "$LATEST_NOT_PROCESSED_PATH" -c:v libx264 -t "$FRAME_DURATION" -pix_fmt yuv420p "$NEW_FRAME_VIDEO_PATH"; then
       rm "$NEW_FRAME_VIDEO_PATH" 2> /dev/null
       continue
