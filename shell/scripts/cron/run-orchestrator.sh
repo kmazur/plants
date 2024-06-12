24905abcfc7fb20baa34579efa27d180aa537ae7
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2024-06-13 00:42:59 +0200

    scheduler

--- shell/scripts/cron/run-orchestrator.sh
+++ shell/scripts/cron/run-orchestrator.sh
@@ -9,12 +9,13 @@ MAX_TEMP=78
 MIN_TEMP=50
 BASE_TOKENS=100
 MAX_TOKENS=100
-SLEEP_INTERVAL=1
+REPLENISH_RATE=5.0  # tokens per second
 
 ensure_file_exists "$SCHEDULER_FILE"
 
 available_tokens=$BASE_TOKENS
-replenish_rate=10.0
+replenish_rate=$REPLENISH_RATE
+last_replenish_time=$(date +%s.%N)
 
 function parse_scheduler_file() {
     local file="$1"
@@ -40,21 +41,25 @@ function wake_up_process() {
 function adjust_replenish_rate() {
     local temp="$1"
     if (( $(echo "$temp < $MIN_TEMP" | bc -l) )); then
-        replenish_rate=10.0
+        replenish_rate=$REPLENISH_RATE
     elif (( $(echo "$temp >= $MAX_TEMP" | bc -l) )); then
         replenish_rate=0.0
     else
-        replenish_rate=$(echo "scale=2; 10.0 - (9.0 * ($temp - $MIN_TEMP) / ($MAX_TEMP - $MIN_TEMP))" | bc)
+        replenish_rate=$(echo "scale=2; $REPLENISH_RATE - (4.5 * ($temp - $MIN_TEMP) / ($MAX_TEMP - $MIN_TEMP))" | bc)
     fi
 }
 
 function replenish_tokens() {
-    if (( $(echo "$available_tokens < $MAX_TOKENS" | bc -l) )) && (( $(echo "$replenish_rate > 0" | bc -l) )); then
-        available_tokens=$(echo "$available_tokens + $replenish_rate" | bc)
-        if (( $(echo "$available_tokens > $MAX_TOKENS" | bc -l) )); then
-            available_tokens=$MAX_TOKENS
-        fi
+    local current_time=$(date +%s.%N)
+    local elapsed_time=$(echo "$current_time - $last_replenish_time" | bc)
+    local tokens_to_add=$(echo "$elapsed_time * $replenish_rate" | bc)
+    available_tokens=$(echo "$available_tokens + $tokens_to_add" | bc)
+
+    if (( $(echo "$available_tokens > $MAX_TOKENS" | bc -l) )); then
+        available_tokens=$MAX_TOKENS
     fi
+
+    last_replenish_time=$current_time
 }
 
 function run_scheduler() {
@@ -69,6 +74,7 @@ function run_scheduler() {
     local total_tokens=0
 
     for entry in "${entries[@]}"; do
+        # entries+=("$process:$tokens:$timestamp:$sleep_pid")
         local process=$(echo "$entry" | cut -d: -f1)
         local tokens=$(echo "$entry" | cut -d: -f2)
         local timestamp=$(echo "$entry" | cut -d: -f3)
@@ -93,7 +99,7 @@ while true; do
     current_temp=$(get_cpu_temp)
     adjust_replenish_rate "$current_temp"
 
-    # Replenish tokens
+    # Replenish tokens based on elapsed time
     replenish_tokens
 
     # Run the scheduler
@@ -101,6 +107,4 @@ while true; do
 
     # Log available tokens and replenish rate
     log "Available tokens: $available_tokens, Replenish rate: $replenish_rate"
-
-    #sleep "$SLEEP_INTERVAL"
 done
