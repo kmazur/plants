bbf42b26ed05747956594547ce8f3a21fbd3cb8c
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2024-06-14 20:41:12 +0200

    fix

--- shell/scripts/init/init-cron.sh
+++ shell/scripts/init/init-cron.sh
@@ -6,9 +6,9 @@ source "$LIB_INIT_FILE"
 ensure_env
 
 # Ensure crontab entries are present
-add_crontab_entry '0 0 * * * sudo reboot -h now'
-add_crontab_entry '*/5 * * * * /home/user/WORK/workspace/plants/shell/cron/run_periodic_check.sh "cleanup" 300 1800'
-add_crontab_entry '*/5 * * * * /home/user/WORK/workspace/plants/shell/cron/run_periodic_check.sh "temp-fail-safe" 20 60'
-add_crontab_entry '*/5 * * * * /home/user/WORK/workspace/plants/shell/cron/run_periodic_check.sh "report-cpu-temp" 20 120'
-add_crontab_entry '*/5 * * * * /home/user/WORK/workspace/plants/shell/cron/run_periodic_check.sh "process-influx-queue" 10 500'
+#add_crontab_entry '0 0 * * * sudo reboot -h now'
+#add_crontab_entry '*/5 * * * * /home/user/WORK/workspace/plants/shell/cron/run_periodic_check.sh "cleanup" 300 1800'
+#add_crontab_entry '*/5 * * * * /home/user/WORK/workspace/plants/shell/cron/run_periodic_check.sh "temp-fail-safe" 20 60'
+#add_crontab_entry '*/5 * * * * /home/user/WORK/workspace/plants/shell/cron/run_periodic_check.sh "report-cpu-temp" 20 120'
+#add_crontab_entry '*/5 * * * * /home/user/WORK/workspace/plants/shell/cron/run_periodic_check.sh "process-influx-queue" 10 500'
 
