6ac599edcc9c3c8f19b439fe8e5e70fceda2bc04
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2024-06-24 23:08:01 +0200

    fix

--- shell/scripts/cron/run_all_periodics.sh
+++ shell/scripts/cron/run_all_periodics.sh
@@ -12,6 +12,26 @@ fi
 COMMANDS="$(cat "$CRON_FILE" | grep "run_periodic_check.sh" | grep -v "^#" | cut -d ' ' -f 6-)"
 PROCESS_NAMES="$(echo "$COMMANDS" | cut -d' ' -f 2 | xargs -I {} echo "$(strip "{}")")"
 
+ANY_RUNNING="false"
+
+for NAME in $PROCESS_NAMES; do
+  PROCESS_NAME="$NAME"
+  LOCK_FILE="$LOCKS_DIR/$PROCESS_NAME.lock"
+  PID_FILE="$LOCKS_DIR/$PROCESS_NAME.pid"
+  if [ -f "$PID_FILE" ]; then
+    PID=$(cat "$PID_FILE")
+    if ps -p "$PID" > /dev/null; then
+      ANY_RUNNING="true"
+      break
+    fi
+  fi
+done
+
+if [[ "$ANY_RUNNING" == "false" ]]; then
+  # restart
+  rm /dev/shm/REQUESTS.txt &> /dev/null
+fi
+
 for NAME in $PROCESS_NAMES; do
   PROCESS_NAME="$NAME"
   LOCK_FILE="$LOCKS_DIR/$PROCESS_NAME.lock"
